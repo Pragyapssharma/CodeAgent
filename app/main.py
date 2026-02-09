a98fff5bd654d15d4b8ec7a28dc509379fe087fe
pragya sharma <pragyasharma1209@gmail.com>
2026-02-09 11:55:53 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -49,8 +49,7 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!", file=sys.stderr)
 
-    # TODO: Uncomment the following line to pass the first stage
-    print(chat.choices[0].message.content)
+
 
     # Get the first choice's message
     message = chat.choices[0].message
